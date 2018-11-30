02c6e8b56b4f578f210727f076f930df5f5a2773
Krishnanunni <krishnanunnir97@gmail.com>
2018-11-30 15:29:07 +0530

    Moved the secret key to secret.py at root of the directory

--- mcqfoss/settings.py
+++ mcqfoss/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from secret import SECRET_KEY
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'kqr&z%mm97n&=11jfd&)x%3(xb5fq2_o7=uv_9xv$h^x_j#e@r'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
