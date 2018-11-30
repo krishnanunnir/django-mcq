42c69924c2167e2bf12fb1b761ec03425bcf1566
Krishnanunni <krishnanunnir97@gmail.com>
2018-11-30 15:33:04 +0530

    Moved the database data to the database.py file located at root

--- mcqfoss/settings.py
+++ mcqfoss/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 from secret import SECRET_KEY
+from database import DATABASES
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -77,12 +78,7 @@ WSGI_APPLICATION = 'mcqfoss.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+
 
 
 # Internationalization
